0f51656b959f5555f0ee621af4953aafd58a4613
Gargi-1234 <87595595+Gargi-1234@users.noreply.github.com>
2022-08-09 14:11:58 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,73 @@
+
+const dictionary = {
+  "the" : {
+    "word" : "the",
+    "lexicalCategory":"Determiner",
+    "definition" : "denoting one or more people or things already mentioned or assumed to be common knowledge.",
+    "example" : "THE kid was crying.",
+  },
+  "india" : {
+    "word" : "India",
+    "lexicalCategory":"Noun",
+    "definition" :"a country in southern Asia occupying the greater part of the Indian subcontinent; population 1,311,000,000 (estimated 2015); official languages, Hindi and English (fourteen other languages are recognized as official in certain regions; of these, Bengali, Gujarati, Marathi, Tamil, Telugu, and Urdu have most first-language speakers); capital, New Delhi.",
+    "example" : "INDIA is the largest democratic country in the world.",
+  },
+  "externally" : {
+    "word" : "externally",
+    "lexicalCategory":"Adverb",
+    "definition" : "with reference to the outer surface or structure of something; outside",
+    "example" :  "Most exam papers are marked EXTERNALLY.",
+
+  },
+  "hello" : {
+    "word" : "hello",
+    "lexicalCategory":"Interjection",
+    "definition" : "used as a greeting or to begin a telephone conversation",
+    "example" : "Hello",
+   
+  },
+  "welcome" : {
+    "word" : "welcome",
+    "lexicalCategory":"Noun",
+    "definition" : "an instance or manner of greeting someone",
+    "example" : "Hotels should WELCOME guests in their own language.",
+  
+  },
+  "almost" : {
+    "word" : "almost",
+    "lexicalCategory":"Adverb",
+    "definition" : "not quite; very nearly",
+    "example" : "He ALMOST knocked Georgina over",
+  
+  },
+  "she" : {
+    "word" : "she",
+    "lexicalCategory":"Pronoun",
+    "definition" : "used to refer to a woman, girl, or female animal previously mentioned or easily identified",
+    "example" : "SHE has completed all the work given to her",
+  
+  },
+  "tree" : {
+    "word" : "tree",
+    "lexicalCategory":"Noun",
+    "definition" : "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground.",
+    "example" : "Wow! what a beautiful TREE.",
+   
+  },
+  "wonderful" : {
+    "word" : "wonderful",
+    "lexicalCategory":"Adjective",
+    "definition" : "inspiring delight, pleasure, or admiration; extremely good; marvellous",
+    "example" : "The climate was WONDERFUL all the year round.",
+   
+  },
+  "ordinary" : {
+    "word" : "ordinary",
+    "lexicalCategory":"Adjective",
+    "definition" : "with no special or distinctive features; normal",
+    "example" : "It was just an ORDINARY evening.",
+  
+  }
+}
+
+export default dictionary
---
+++ screens/MoreEx.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+
+export default class MoreEx extends Component{
+render(){
+    return(
+        <View>
+            <Text style={styles.styles} > India </Text>
+        </View>
+    )
+}
+}
+const styles = StyleSheet.create({
+    textStyle: {
+        textAlign: 'center',
+        color: 'black',
+        fontWeight: 'bold',
+      },
+    
+})
